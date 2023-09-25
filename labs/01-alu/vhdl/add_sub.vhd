850d012bcf3a7169ac83cf8e9cc99b4d46df3503
eric.dallacqua <eric.dallacqua@epfl.ch>
2023-09-25 14:54:17 +0200

    Working on addsub

--- labs/01-alu/vhdl/add_sub.vhd
+++ labs/01-alu/vhdl/add_sub.vhd
@@ -14,25 +14,40 @@ entity add_sub is
 end add_sub;
 
 architecture synth of add_sub is
+    signal sub_32 : std_logic_vector(31 downto 0);
+    signal r_out  : std_logic_vector(32 downto 0);
 begin
-    logic : process(a, b, sub_mode)
+    with sub_mode select sub_32 <= x"FFFFFFFF" when '1',
+                                   x"00000000" when others;
+
+    logic : process(a, b, sub_32, sub_mode)
     begin
-        if sub_mode = '1' then
-            r <= std_logic_vector(unsigned(a) - unsigned(b));
+        r_out <= a or (b xor sub_32) or std_logic_vector(sub_mode);
 
-            if unsigned(a) - unsigned(b) = 0 then
+        if r_out = std_logic_vector(0) then
                 zero <= '1';
             else
                 zero <= '0';
-            end if;
-        else
-            r <= std_logic_vector(unsigned(a) + unsigned(b));
-
-            if unsigned(a) + unsigned(b) = 0 then
-                zero <= '1';
-            else
-                zero <= '0';
-            end if;
         end if;
+
+        r <= r_out;
+
+        -- if sub_mode = '1' then
+        --     r <= std_logic_vector(unsigned(a) - unsigned(b));
+
+        --     if unsigned(a) - unsigned(b) = 0 then
+        --         zero <= '1';
+        --     else
+        --         zero <= '0';
+        --     end if;
+        -- else
+        --     r <= std_logic_vector(unsigned(a) + unsigned(b));
+
+        --     if unsigned(a) + unsigned(b) = 0 then
+        --         zero <= '1';
+        --     else
+        --         zero <= '0';
+        --     end if;
+        -- end if;
     end process logic;
 end synth;
