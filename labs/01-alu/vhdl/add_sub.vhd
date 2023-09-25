63ac56e48d99a4bd29e5a284a398a68cc406abe8
eric.dallacqua <eric.dallacqua@epfl.ch>
2023-09-25 18:39:16 +0200

    Finished addsub needs testing

--- labs/01-alu/vhdl/add_sub.vhd
+++ labs/01-alu/vhdl/add_sub.vhd
@@ -20,9 +20,9 @@ begin
     with sub_mode select sub_32 <= x"FFFFFFFF" when '1',
                                    x"00000000" when others;
 
-    logic : process(a, b, sub_32, sub_mode)
+    logic : process(a, b, sub_32, sub_mode, r_out)
     begin
-        r_out <= a or (b xor sub_32) or std_logic_vector(sub_mode);
+        r_out <= std_logic_vector( unsigned(a) + unsigned(b xor sub_32) + unsigned(sub_mode) );
 
         if r_out = std_logic_vector(0) then
                 zero <= '1';
@@ -30,24 +30,9 @@ begin
                 zero <= '0';
         end if;
 
-        r <= r_out;
+        carry <= r_out(32);
 
-        -- if sub_mode = '1' then
-        --     r <= std_logic_vector(unsigned(a) - unsigned(b));
-
-        --     if unsigned(a) - unsigned(b) = 0 then
-        --         zero <= '1';
-        --     else
-        --         zero <= '0';
-        --     end if;
-        -- else
-        --     r <= std_logic_vector(unsigned(a) + unsigned(b));
-
-        --     if unsigned(a) + unsigned(b) = 0 then
-        --         zero <= '1';
-        --     else
-        --         zero <= '0';
-        --     end if;
-        -- end if;
+        r <= r_out(31 downto 0);
+        
     end process logic;
 end synth;
