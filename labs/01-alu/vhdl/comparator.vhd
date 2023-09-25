1e91be1bdb51a85819c6b784835ddc95c42ce8c0
Arthur Wolf <arthur.wlf@icloud.com>
2023-09-25 14:50:05 +0200

    Finish comparator and start tb_comparator

--- labs/01-alu/vhdl/comparator.vhd
+++ labs/01-alu/vhdl/comparator.vhd
@@ -15,23 +15,10 @@ end comparator;
 
 architecture synth of comparator is
 begin
-    logic : process(op)
-    begin
-        case op is 
-            when "011001" => -- signed less or equal
-
-            when "011010" => -- signed greater
-
-            when "011011" => -- comparison : is not equal
-
-            when "011100" => -- comparison : is equal
-
-            when "011101" => -- unsigned less or equal
-
-            when "011110" => -- unsigned greater
-            
-            when others =>
-                r <= 'X';
-        end case;
-    end process logic;
+    r <= '1' when (op = "001" and ((a_31 = '1' and b_31 = '0') or ((a_31 = '1' xnor b_31 = '1') and (diff_31 = '1' or zero = '1')))) or   -- A <= B signed
+                  (op = "010" and ((a_31 = '0' and b_31 = '1') or ((a_31 = '1' xnor b_31 = '1') and (diff_31 = '0' and zero = '0')))) or  -- A > B signed
+                  ((op = "011" or op = "000" or op = "111") and zero = '0') or                            -- A /= B
+                  (op = "100" and zero = '1') or                            -- A = B
+                  (op = "101" and (carry = '0' or zero = '1')) or           -- A <= B unsigned
+                  (op = "110" and (carry = '1' and zero = '0')) else '0';   -- A > B unsigned
 end synth;
