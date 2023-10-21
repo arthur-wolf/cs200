03945ff91b92be3099e344002f0963716865c4c1
Arthur Wolf <arthur.wlf@icloud.com>
2023-10-21 16:12:26 +0200

    Finish writing constants

--- labs/03-cpu/vhdl/controller.vhd
+++ labs/03-cpu/vhdl/controller.vhd
@@ -41,17 +41,64 @@ architecture synth of controller is
     signal s_current_state, s_next_state : state:= FETCH1;
     signal s_op, s_opx : std_logic_vector(5 downto 0);
 
+    -- R-type operations + opx
     constant c_rtype_op : std_logic_vector(5 downto 0) := "111010"; -- 0x3A
-    constant c_store : std_logic_vector(5 downto 0) := "010101"; -- 0x15
-    constant c_break : std_logic_vector(5 downto 0) := "110100"; -- 0x34
-    constant c_load1 : std_logic_vector(5 downto 0) := "010111"; -- 0x17
-    constant c_itype_op : std_logic_vector(5 downto 0) := "111110"; -- 0x3E
-
-    constant c_and : std_logic_vector(5 downto 0) := "001110"; -- 0x0E
-    constant c_srl : std_logic_vector(5 downto 0) := "011011"; -- 0x1B
-
-    constant c_addi : std_logic_vector(5 downto 0) := "000100"; -- 0x04
     
+    constant c_break : std_logic_vector(5 downto 0) := "110100"; -- 0x34
+    constant c_callr : std_logic_vector(5 downto 0) := "011101"; -- 0x1D
+    constant c_jmp : std_logic_vector(5 downto 0) := "001101"; -- 0x0D
+    constant c_ret : std_logic_vector(5 downto 0) := "000101"; -- 0x05
+
+    constant c_add : std_logic_vector(5 downto 0) := "110001"; -- 0x31
+    constant c_sub : std_logic_vector(5 downto 0) := "111001"; -- 0x39
+    constant c_cmple : std_logic_vector(5 downto 0) := "001000"; -- 0x08
+    constant c_cmpgt : std_logic_vector(5 downto 0) := "010000"; -- 0x10
+    constant c_nor : std_logic_vector(5 downto 0) := "000110"; -- 0x06
+    constant c_and : std_logic_vector(5 downto 0) := "001110"; -- 0x0E
+    constant c_or : std_logic_vector(5 downto 0) := "010110"; -- 0x16
+    constant c_xnor : std_logic_vector(5 downto 0) := "011110"; -- 0x1E
+    constant c_sll : std_logic_vector(5 downto 0) := "010011"; -- 0x13
+    constant c_srl : std_logic_vector(5 downto 0) := "011011"; -- 0x1B
+    constant c_sra : std_logic_vector(5 downto 0) := "111011"; -- 0x3B
+
+    constant c_slli : std_logic_vector(5 downto 0) := "010010"; -- 0x12
+    constant c_srli : std_logic_vector(5 downto 0) := "011010"; -- 0x1A
+    constant c_srai : std_logic_vector(5 downto 0) := "111010"; -- 0x3A
+    constant c_roli : std_logic_vector(5 downto 0) := "000010"; -- 0x02
+
+    constant c_cmpne : std_logic_vector(5 downto 0) := "011000"; -- 0x18
+    constant c_cmpeq : std_logic_vector(5 downto 0) := "100000"; -- 0x20
+    constant c_cmpleu : std_logic_vector(5 downto 0) := "101000"; -- 0x28
+    constant c_cmpgtu : std_logic_vector(5 downto 0) := "110000"; -- 0x30
+    constant c_rol : std_logic_vector(5 downto 0) := "000011"; -- 0x03
+    constant c_ror : std_logic_vector(5 downto 0) := "001011"; -- 0x0B
+    
+    -- I-type operations
+    constant c_itype_op : std_logic_vector(5 downto 0) := "111110"; -- 0x3E
+    constant c_store : std_logic_vector(5 downto 0) := "010101"; -- 0x15
+    constant c_load1 : std_logic_vector(5 downto 0) := "010111"; -- 0x17
+    constant c_jumpi : std_logic_vector(5 downto 0) := "000001"; -- 0x01
+    constant c_addi : std_logic_vector(5 downto 0) := "000100"; -- 0x04
+    constant c_andi : std_logic_vector(5 downto 0) := "001100"; -- 0x0C
+    constant c_ori : std_logic_vector(5 downto 0) := "010100"; -- 0x14
+    constant c_xnori : std_logic_vector(5 downto 0) := "011100"; -- 0x1C
+    constant c_call : std_logic_vector(5 downto 0) := "000000"; -- 0x00
+    constant c_cmplei : std_logic_vector(5 downto 0) := "001000"; -- 0x08
+    constant c_cmpgti : std_logic_vector(5 downto 0) := "010000"; -- 0x10
+    constant c_cmpnei : std_logic_vector(5 downto 0) := "011000"; -- 0x18
+    constant c_cmpeqi : std_logic_vector(5 downto 0) := "100000"; -- 0x20
+    constant c_cmpleui : std_logic_vector(5 downto 0) := "101000"; -- 0x28
+    constant c_cmpgtui : std_logic_vector(5 downto 0) := "110000"; -- 0x30
+
+    -- branch op
+    constant c_br : std_logic_vector(5 downto 0) := "000110"; -- 0x06
+    constant c_ble : std_logic_vector(5 downto 0) := "001110"; -- 0x0E
+    constant c_bgt : std_logic_vector(5 downto 0) := "010110"; -- 0x16
+    constant c_bne : std_logic_vector(5 downto 0) := "011110"; -- 0x1E
+    constant c_beq : std_logic_vector(5 downto 0) := "100110"; -- 0x26
+    constant c_bleu : std_logic_vector(5 downto 0) := "101110"; -- 0x2E
+    constant c_bgtu : std_logic_vector(5 downto 0) := "110110"; -- 0x36
+
 begin
     s_op <= op;
     s_opx <= opx;
