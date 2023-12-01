f914f5cde91db491734f59b5fa43ccd7c4270ebe
eric.dallacqua <eric.dallacqua@epfl.ch>
2023-12-01 16:04:00 +0100

    Correction on lab 1

--- labs/05-timer/part-1/vhdl/timer.vhd
+++ labs/05-timer/part-1/vhdl/timer.vhd
@@ -30,21 +30,21 @@ begin
 
     irq <= '1' when ( s_TO = '1' and s_ITO = '1' ) else '0';
 
-    read : Process(clk, reset_n)
+    read_process : Process(clk, reset_n)
     begin
         if reset_n = '0' then
             to_write <= (others => '0');
         else if rising_edge(clk) then
             if ( read = '1' ) and ( cs = '1' ) then
                 case address is
-                    when '00' =>
+                    when "00" =>
                         to_write <= s_counter;
-                    when '01' =>
+                    when "01" =>
                         to_write <= s_period;
-                    when '10' =>
-                        to_write <= 0x00000000 or (s_ITO & s_CONT);
-                    when '11' =>
-                        to_write <= 0x00000000 or (s_TO & s_RUN);
+                    when "10" =>
+                        to_write <= "0x00000000" or (s_ITO & s_CONT);
+                    when "11" =>
+                        to_write <= "0x00000000" or (s_TO & s_RUN);
                     when others =>
                         to_write <= (others => '0');
                 end case;
@@ -53,9 +53,10 @@ begin
                 rddata <= (others => 'X');
             end if;
         end if;
-    end process read;
+        end if;
+    end process read_process;
 
-    write : Process(clk, reset_n)
+    write_process : Process(clk, reset_n)
     begin
         if reset_n = '0' then
             s_counter <= (others => '0');
@@ -67,29 +68,29 @@ begin
         else if rising_edge(clk) then
             if ( write = '1' ) and ( cs = '1' ) then
                 case address is
-                    when '00' =>
+                    when "00" =>
                         s_counter <= wrdata;
-                    when '01' =>
+                    when "01" =>
                         s_period <= wrdata;
                         s_counter <= wrdata;
                         s_RUN <= '0';
-                    when '10' =>
-                        if RUN = '0' and wrdata(3) = '1' then
-                            RUN <= '1';            
+                    when "10" =>
+                        if s_RUN = '0' and wrdata(3) = '1' then
+                            s_RUN <= '1';            
                         end if;
-                        if RUN = '1' and wrdata(2) = '1' then
-                            RUN <= '0';
+                        if s_RUN = '1' and wrdata(2) = '1' then
+                            s_RUN <= '0';
                         end if;
-                        s_ITO <= wrdata(1)
-                        s_CONT <= wrdata(0)
-                    when '11' =>
-                        s_TO <= wrdata(1)
-                        s_RUN <= wrdata(0)
+                        s_ITO <= wrdata(1);
+                        s_CONT <= wrdata(0);
+                    when "11" =>
+                        s_TO <= wrdata(1);
+                        s_RUN <= wrdata(0);
                     when others =>
                         null;
                 end case;
             end if;
-            if RUN = '1' then
+            if s_RUN = '1' then
                 s_counter <= next_counter;
                 if s_counter = (others => '0') then
                     s_TO <= '1';
@@ -97,7 +98,9 @@ begin
                         s_RUN <= '0';
                     end if;
                 end if;
+            end if;
         end if;
-    end process write;
+        end if;
+    end process write_process;
 
 end synth;
