c3d258d9a111bf8b5caa0236b64c96d41f0ea97a
eric.dallacqua <eric.dallacqua@epfl.ch>
2023-12-06 13:59:27 +0100

    Fix lab1

--- labs/05-timer/part-1/vhdl/timer.vhd
+++ labs/05-timer/part-1/vhdl/timer.vhd
@@ -20,87 +20,73 @@ end timer;
 
 architecture synth of timer is
 
-    signal s_TO, s_RUN, s_ITO, s_CONT                       : std_logic;
-    signal s_counter, s_period                              : std_logic_vector(31 downto 0);
-    signal next_counter, to_write                           : std_logic_vector(31 downto 0);
+    signal s_counter, s_period, s_control, s_status : std_logic_vector(31 downto 0);
 
+    signal s_read: std_logic;   
 begin
+    rddata <= 
+        s_counter when (s_read = '1' and address = "00") else 
+        s_period when (s_read = '1' and address = "01") else 
+        s_control when (s_read = '1' and address = "10") else 
+        s_status when (s_read = '1' and address = "11") else 
+        (others => 'Z');
 
-    next_counter <= std_logic_vector(unsigned(s_counter) - 1) when not( unsigned(s_counter) = 0 ) else s_period;
+    irq <= '1' when (s_control(1) = '1' and s_status(1) = '1') else '0';
 
-    irq <= '1' when ( s_TO = '1' and s_ITO = '1' ) else '0';
-
-    read_process : Process(clk, reset_n)
+    write_flipflop: process(reset_n, clk) is 
     begin
-        if reset_n = '0' then
-            to_write <= (others => '0');
-        else if rising_edge(clk) then
-            if ( read = '1' ) and ( cs = '1' ) then
-                case address is
-                    when "00" =>
-                        to_write <= s_counter;
-                    when "01" =>
-                        to_write <= s_period;
-                    when "10" =>
-                        to_write <= "00000000000000000000000000000000" or (s_ITO & s_CONT);
-                    when "11" =>
-                        to_write <= "00000000000000000000000000000000" or (s_TO & s_RUN);
-                    when others =>
-                        to_write <= (others => '0');
-                end case;
-                rddata <= to_write;
-            else
-                rddata <= (others => 'Z');
-            end if;
-        end if;
-        end if;
-    end process read_process;
 
-    write_process : Process(clk, reset_n)
-    begin
-        if reset_n = '0' then
+        if (reset_n = '0') then 
+
             s_counter <= (others => '0');
             s_period <= (others => '0');
-            s_ITO     <= '0';
-            s_CONT    <= '0';
-            s_TO      <= '0';
-            s_RUN     <= '0';
-        else if rising_edge(clk) then
-            if ( write = '1' ) and ( cs = '1' ) then
-                case address is
-                    when "00" =>
-                        s_counter <= wrdata;
-                    when "01" =>
-                        s_period <= wrdata;
-                        s_counter <= wrdata;
-                        s_RUN <= '0';
-                    when "10" =>
-                        if s_RUN = '0' and wrdata(3) = '1' then
-                            s_RUN <= '1';            
-                        end if;
-                        if s_RUN = '1' and wrdata(2) = '1' then
-                            s_RUN <= '0';
-                        end if;
-                        s_ITO <= wrdata(1);
-                        s_CONT <= wrdata(0);
-                    when "11" =>
-                        s_TO <= wrdata(1);
-                        s_RUN <= wrdata(0);
-                    when others =>
-                        null;
-                end case;
-            end if;
-            if s_RUN = '1' then
-                s_counter <= next_counter;
-                if unsigned(s_counter) = 0 then
-                    s_TO <= '1';
-                    if s_CONT = '1' then
-                        s_RUN <= '0';
+            s_control <= (others => '0');
+            s_status <= (others => '0');
+
+        elsif(rising_edge(clk)) then 
+
+            s_read <= cs and read;
+
+            if(s_status(0) = '1') then 
+                s_counter <= std_logic_vector(unsigned(s_counter) - 1);
+
+                if((to_integer(unsigned(s_counter))) = 1) then
+                    s_status(1) <= '1';
+                end if;
+    
+                if((to_integer(unsigned(s_counter))) = 0) then 
+                    s_counter <= s_period;
+                    if(s_control(0) = '0') then
+                    s_status(0) <= '0';
                     end if;
                 end if;
+            end if; 
+    
+            if(cs = '1' AND write = '1') then 
+                case address is 
+
+                    when "01" => 
+                        s_period <= wrdata; 
+                        s_counter <= wrdata;
+                        s_status(0) <= '0';
+
+                    when "10" => 
+                        s_control(1 downto 0) <= wrdata(1 downto 0); 
+                        if(wrdata(3) = '1' AND s_status(0) = '0') then
+                            s_status(0) <= '1';
+                        elsif(wrdata(2) = '1' AND s_status(0) = '1') then
+                            s_status(0) <= '0';
+                        end if;
+
+                    when "11" => 
+                        if(wrdata(1) = '0') then
+                            s_status(1) <= '0';
+                        end if;
+
+                    when OTHERS => null;
+
+                end case;
             end if;
         end if;
-        end if;
-    end process write_process;
-
+    end process write_flipflop;
 end synth;
